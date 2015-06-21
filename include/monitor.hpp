902c284bf977614cba8aa96d09c15e78a880f092
Mlordx <matbarrosrodrigues@gmail.com>
2015-06-21 11:56:59 -0300

    Move header files to include/

---
+++ include/monitor.hpp
@@ -0,0 +1,25 @@
+#ifndef MONITOR_HPP
+#define MONITOR_HPP
+#include <condition_variable>
+#include <thread>
+#include <vector>
+#include <mutex>
+
+using namespace std;
+
+class Monitor
+{
+    condition_variable cv;
+    mutex m;
+
+    void signal();
+
+    void wait(unique_lock<mutex>&);
+    
+  public:
+
+    void devolveGarfo();
+
+    void requisitaGarfo();
+};
+#endif
