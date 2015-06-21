c2d15188555bde4608ca0c5117083de0d916ebb6
karina <k.sueminha@gmail.com>
2015-06-21 17:11:50 -0300

    Passando as funções para o ,cpp .. pois no .hpp ele não reconhecia atributo da classe

--- include/monitor.hpp
+++ include/monitor.hpp
@@ -1,5 +1,7 @@
 #ifndef MONITOR_HPP
 #define MONITOR_HPP
+#define DEBUG 1
+
 #include <condition_variable>
 #include <thread>
 #include <vector>
@@ -10,6 +12,7 @@ using namespace std;
 
 class Monitor
 {	
+    int quantosEsperam;
     condition_variable cv;
     mutex m;
 
@@ -19,8 +22,33 @@ class Monitor
     
   public:
 
-    void devolveGarfo();
+  	Monitor()
+  	{
+  		quantosEsperam = 0;
+		};
 
-    void requisitaGarfo();
+    void devolveGarfo(int apaga)
+    {
+    	if(DEBUG) printf("devolveGarfo - entra %d\n", apaga);
+			this->quantosEsperam--;
+		  unique_lock<mutex> lck(m);
+		  this->signal();
+    	if(DEBUG) printf("devolveGarfo - saída %d\n", apaga);
+		};
+
+    void requisitaGarfo(int apaga)
+    {
+    	if(DEBUG) printf("requisitaGarfo - entra %d\n", apaga);
+			while(quantosEsperam > 0)
+			{
+				printf("  1\n");
+		    unique_lock<mutex> lck(m);
+				printf("  2\n");
+		    this->wait(lck);
+				printf("  3\n");
+			}
+			quantosEsperam++;
+    	if(DEBUG) printf("requisitaGarfo - saída %d\n", apaga);
+		};
 };
 #endif
