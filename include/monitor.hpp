b1ae0722bcf6555d063107957ea059c1a8b12a0e
Mlordx <matbarrosrodrigues@gmail.com>
2015-06-21 21:27:07 -0300

    implementações colocadas no .cpp

--- include/monitor.hpp
+++ include/monitor.hpp
@@ -11,44 +11,21 @@
 using namespace std;
 
 class Monitor
-{	
-    int quantosEsperam;
-    condition_variable cv;
-    mutex m;
+{
+  int quantosEsperam;
+  condition_variable cv;
+  mutex m;
+  
+  void signal();
+  
+  void wait(unique_lock<mutex>&);
+  
+public:
+  
+  Monitor(): quantosEsperam(0){};
 
-    void signal();
+  void devolveGarfo(int);
 
-    void wait(unique_lock<mutex>&);
-    
-  public:
-
-  	Monitor()
-  	{
-  		quantosEsperam = 0;
-		};
-
-    void devolveGarfo(int apaga)
-    {
-    	if(DEBUG) printf("devolveGarfo - entra %d\n", apaga);
-			this->quantosEsperam--;
-		  unique_lock<mutex> lck(m);
-		  this->signal();
-    	if(DEBUG) printf("devolveGarfo - saída %d\n", apaga);
-		};
-
-    void requisitaGarfo(int apaga)
-    {
-    	if(DEBUG) printf("requisitaGarfo - entra %d\n", apaga);
-			while(quantosEsperam > 0)
-			{
-				printf("  1\n");
-		    unique_lock<mutex> lck(m);
-				printf("  2\n");
-		    this->wait(lck);
-				printf("  3\n");
-			}
-			quantosEsperam++;
-    	if(DEBUG) printf("requisitaGarfo - saída %d\n", apaga);
-		};
+  void requisitaGarfo(int);
 };
 #endif
--- src/monitor.cpp
+++ src/monitor.cpp
@@ -9,3 +9,27 @@ void Monitor::wait(unique_lock<mutex> &lck)
 {
   cv.wait(lck);
 }
+
+void Monitor::devolveGarfo(int apaga)
+{ 
+    if(DEBUG) printf("devolveGarfo - entra %d\n", apaga);
+    this->quantosEsperam--;
+    this->signal();
+    if(DEBUG) printf("devolveGarfo - saída %d\n", apaga);
+}
+
+void Monitor::requisitaGarfo(int apaga)
+{
+  
+  if(DEBUG) printf("requisitaGarfo - entra %d\n", apaga);
+  while(this->quantosEsperam > 0)
+    {
+      printf("  1\n");
+      unique_lock<mutex> lck(m);
+      printf("  2\n");
+      this->wait(lck);
+      printf("  3\n");
+    }
+  this->quantosEsperam++;
+  if(DEBUG) printf("requisitaGarfo - saída %d\n", apaga);
+}
